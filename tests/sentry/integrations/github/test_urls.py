5783b2791b31fd20cc4eb4cbd21cd8b6f2442026
mdtro <matthew.trostel@sentry.io>
2022-04-25 18:09:19 -0500

    unit tests for url matching of github integration routes

---
+++ tests/sentry/integrations/github/test_urls.py
@@ -0,0 +1,30 @@
+import pytest
+from django.urls import resolve
+
+# tests sentry.integrations.github.urls
+# included in top-level route as "extensions/github/.."
+
+PATHS_TO_ROUTE_NAMES = [
+    ("/extensions/github/webhook", "sentry-extensions-github-webhook"),
+    ("/extensions/github/search/orgslug/1234", "sentry-extensions-github-search"),
+]
+
+
+@pytest.mark.url_resolve
+@pytest.mark.parametrize("path,expected_route_name", PATHS_TO_ROUTE_NAMES)
+def test_expected_route_match_without_trailing_slash(path: str, expected_route_name):
+    path = path.rstrip("/")  # removing any trailing slash, we want to test without it
+    match = resolve(f"{path}")
+    assert (
+        match.url_name == expected_route_name
+    ), f"{match.url_name} != {expected_route_name} | matched on route {match.route} | matched function: {match._func_path}"
+
+
+@pytest.mark.url_resolve
+@pytest.mark.parametrize("path,expected_route_name", PATHS_TO_ROUTE_NAMES)
+def test_expected_route_match_with_trailing_slash(path: str, expected_route_name):
+    path = path.rstrip("/")  # removing any trailing slash (so we don't end up with `//`)
+    match = resolve(f"{path}/")
+    assert (
+        match.url_name == expected_route_name
+    ), f"{match.url_name} != {expected_route_name} | matched on route {match.route} | matched function: {match._func_path}"
