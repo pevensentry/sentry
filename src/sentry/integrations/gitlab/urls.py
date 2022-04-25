578fd3bcf469f00e02cf9831fb8229edf80eec1d
mdtro <matthew.trostel@sentry.io>
2022-04-25 18:37:56 -0500

    adjust paths for optional trailing slash on gitlab integration; include unit tests

--- src/sentry/integrations/gitlab/urls.py
+++ src/sentry/integrations/gitlab/urls.py
@@ -5,9 +5,9 @@ from .webhooks import GitlabWebhookEndpoint
 
 urlpatterns = [
     url(
-        r"^search/(?P<organization_slug>[^\/]+)/(?P<integration_id>\d+)/$",
+        r"^search/(?P<organization_slug>[^\/]+)/(?P<integration_id>\d+)/?$",
         GitlabIssueSearchEndpoint.as_view(),
         name="sentry-extensions-gitlab-search",
     ),
-    url(r"^webhook/$", GitlabWebhookEndpoint.as_view(), name="sentry-extensions-gitlab-webhook"),
+    url(r"^webhook/?$", GitlabWebhookEndpoint.as_view(), name="sentry-extensions-gitlab-webhook"),
 ]
