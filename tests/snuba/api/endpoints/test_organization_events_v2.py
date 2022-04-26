3bb6cc006bd6264f46c5e9d78414e4deb74087e2
Evan Hicks <evan.hicks@sentry.io>
2022-04-26 12:44:26 -0400

    Discover query builder and visibility team tests

--- tests/snuba/api/endpoints/test_organization_events_v2.py
+++ tests/snuba/api/endpoints/test_organization_events_v2.py
@@ -4770,7 +4770,7 @@ class OrganizationEventsV2EndpointTest(APITestCase, SnubaTestCase):
 
         assert (
             Function("tuple", ["duration", 300], "project_threshold_config")
-            in mock_snql_query.call_args_list[0][0][0].select
+            in mock_snql_query.call_args_list[0][0][0].query.select
         )
 
     @mock.patch("sentry.search.events.builder.raw_snql_query")
@@ -4873,7 +4873,7 @@ class OrganizationEventsV2EndpointTest(APITestCase, SnubaTestCase):
                 ],
                 "project_threshold_config",
             )
-            in mock_snql_query.call_args_list[0][0][0].select
+            in mock_snql_query.call_args_list[0][0][0].query.select
         )
 
     def test_count_web_vitals(self):
