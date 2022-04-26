7226955832fe9ba4fe3cb76de3b9df1463c910c3
mdtro <matthew.trostel@sentry.io>
2022-04-25 21:41:13 -0500

    prepare vercel and vsts integration urls for django 2.2.28
    - update to use `re_path` instead of `url`
    - trailing slash is now optional in the regex
    - unit tests for route matching

--- src/sentry/integrations/vercel/urls.py
+++ src/sentry/integrations/vercel/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.urls import re_path
 
 from sentry.web.frontend.vercel_extension_configuration import VercelExtensionConfigurationView
 
@@ -6,16 +6,18 @@ from .generic_webhook import VercelGenericWebhookEndpoint
 from .webhook import VercelWebhookEndpoint
 
 urlpatterns = [
-    url(r"^webhook/$", VercelWebhookEndpoint.as_view(), name="sentry-extensions-vercel-webhook"),
-    url(
-        r"^configure/$",
+    re_path(
+        r"^webhook/?$", VercelWebhookEndpoint.as_view(), name="sentry-extensions-vercel-webhook"
+    ),
+    re_path(
+        r"^configure/?$",
         VercelExtensionConfigurationView.as_view(),
         name="sentry-extensions-vercel-configure",
     ),
     # XXX(meredith): This route has become our generic hook, in
     # the future we'll need to update the route name to reflect that.
-    url(
-        r"^delete/$",
+    re_path(
+        r"^delete/?$",
         VercelGenericWebhookEndpoint.as_view(),
         name="sentry-extensions-vercel-generic-webhook",
     ),
---
+++ tests/sentry/integrations/vercel/test_urls.py
@@ -0,0 +1,28 @@
+import pytest
+from django.urls import resolve
+
+PATHS_TO_ROUTE_NAMES = [
+    ("/extensions/vercel/webhook", "sentry-extensions-vercel-webhook"),
+    ("/extensions/vercel/configure", "sentry-extensions-vercel-configure"),
+    ("/extensions/vercel/delete", "sentry-extensions-vercel-generic-webhook"),
+]
+
+
+@pytest.mark.url_resolve
+@pytest.mark.parametrize("path,expected_route_name", PATHS_TO_ROUTE_NAMES)
+def test_expected_route_match_without_trailing_slash(path: str, expected_route_name):
+    path = path.rstrip("/")  # removing any trailing slash, we want to test without it
+    match = resolve(f"{path}")
+    assert (
+        match.url_name == expected_route_name
+    ), f"{match.url_name} != {expected_route_name} | matched on route {match.route} | matched function: {match._func_path}"
+
+
+@pytest.mark.url_resolve
+@pytest.mark.parametrize("path,expected_route_name", PATHS_TO_ROUTE_NAMES)
+def test_expected_route_match_with_trailing_slash(path: str, expected_route_name):
+    path = path.rstrip("/")  # removing any trailing slash (so we don't end up with `//`)
+    match = resolve(f"{path}/")
+    assert (
+        match.url_name == expected_route_name
+    ), f"{match.url_name} != {expected_route_name} | matched on route {match.route} | matched function: {match._func_path}"
