e56ed32d333831f31a4489573227fb729d8d12d0
Billy Vong <billy@sentry.io>
2022-04-28 12:00:01 -0400

    feat(replays): Add highlighting to Replays player
    Adds the ability to highlighting nodes in the replayer as it plays. Currently used to highlight LCP if availble.

--- static/app/views/replays/types.tsx
+++ static/app/views/replays/types.tsx
@@ -8,3 +8,12 @@ export type Replay = {
 };
 
 export type TabBarId = 'console' | 'performance' | 'errors' | 'tags';
+
+/**
+ * Highlight Replay Plugin types
+ */
+export type HighlightsByTime = Map<number, Highlight>;
+export interface Highlight {
+  nodeId: number;
+  color?: string;
+}
